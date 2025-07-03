0cd4986c60024c0f38f14db1fb652f922d7701ce
CodeWithDennis <personal.elsinga@gmail.com>
2025-07-03 16:58:15 +0200

    Add FilamentAdvancedChoiceServiceProvider to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace CodeWithDennis\FilamentAdvancedChoice\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use CodeWithDennis\FilamentAdvancedChoice\FilamentAdvancedChoiceServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use CodeWithDennis\FilamentAdvancedChoice\FilamentAdvancedChoiceServiceProvider;
 
 class TestCase extends Orchestra
 {
