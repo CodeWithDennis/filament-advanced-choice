2f2e7aa6892ecd1ac5d11aa35203177871a389bd
CodeWithDennis <personal.elsinga@gmail.com>
2025-07-03 16:58:20 +0200

    Add HasExtras trait, RadioCards, and RadioTable components

---
+++ src/Filament/Forms/Components/Concerns/HasExtras.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns;
+
+use Closure;
+
+trait HasExtras
+{
+    public Closure | array $extras;
+
+    public function extras(Closure | array $extras): static
+    {
+        $this->extras = $extras;
+
+        return $this;
+    }
+
+    public function getExtras(): array
+    {
+        return $this->evaluate($this->extras);
+    }
+}
