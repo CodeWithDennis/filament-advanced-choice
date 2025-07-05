f999afec647a2f0b4340a3d4dcb558bb377fe2df
CodeWithDennis <personal.elsinga@gmail.com>
2025-07-05 14:54:11 +0200

    Rename RadioCard.php to RadioCards.php and add RadioStackedCards.php

--- src/Filament/Forms/Components/RadioStackedCards.php
+++ src/Filament/Forms/Components/RadioStackedCards.php
@@ -7,9 +7,9 @@ namespace CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components;
 use CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns\HasExtras;
 use Filament\Forms\Components\Radio;
 
-final class RadioCard extends Radio
+final class RadioStackedCards extends Radio
 {
     use HasExtras;
 
-    protected string $view = 'filament-advanced-choice::radio-card';
+    protected string $view = 'filament-advanced-choice::radio-stacked-cards';
 } 
