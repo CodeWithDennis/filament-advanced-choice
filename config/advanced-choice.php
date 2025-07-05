7fd1c502ff09b64e809a85f1f935777d2f673080
CodeWithDennis <personal.elsinga@gmail.com>
2025-07-05 15:35:21 +0200

    Cleanup

--- config/advanced-choice.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// config for CodeWithDennis/FilamentAdvancedChoice
-return [
-
-];
--- src/FilamentAdvancedChoiceServiceProvider.php
+++ src/FilamentAdvancedChoiceServiceProvider.php
@@ -1,22 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace CodeWithDennis\FilamentAdvancedChoice;
 
-use CodeWithDennis\FilamentAdvancedChoice\Commands\FilamentAdvancedChoiceCommand;
-use CodeWithDennis\FilamentAdvancedChoice\Testing\TestsFilamentAdvancedChoice;
-use Filament\Support\Assets\AlpineComponent;
-use Filament\Support\Assets\Asset;
-use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
-use Filament\Support\Facades\FilamentIcon;
-use Illuminate\Filesystem\Filesystem;
-use Livewire\Features\SupportTesting\Testable;
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentAdvancedChoiceServiceProvider extends PackageServiceProvider
+final class FilamentAdvancedChoiceServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-advanced-choice';
 
@@ -24,129 +15,15 @@ class FilamentAdvancedChoiceServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package->name(static::$name)
-            ->hasCommands($this->getCommands())
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations()
-                    ->askToRunMigrations()
-                    ->askToStarRepoOnGitHub('codewithdennis/filament-advanced-choice');
-            });
-
-        $configFileName = $package->shortName();
-
-        if (file_exists($package->basePath("/../config/{$configFileName}.php"))) {
-            $package->hasConfigFile();
-        }
-
-        if (file_exists($package->basePath('/../database/migrations'))) {
-            $package->hasMigrations($this->getMigrations());
-        }
-
-        if (file_exists($package->basePath('/../resources/lang'))) {
-            $package->hasTranslations();
-        }
+        $package->name(self::$name);
 
         if (file_exists($package->basePath('/../resources/views'))) {
-            $package->hasViews(static::$viewNamespace);
+            $package->hasViews(self::$viewNamespace);
         }
-    }
-
-    public function packageRegistered(): void {}
-
-    public function packageBooted(): void
-    {
-        // Asset Registration
-        FilamentAsset::register(
-            $this->getAssets(),
-            $this->getAssetPackageName()
-        );
-
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/filament-advanced-choice/{$file->getFilename()}"),
-                ], 'filament-advanced-choice-stubs');
-            }
-        }
-
-        // Testing
-        Testable::mixin(new TestsFilamentAdvancedChoice);
     }
 
     protected function getAssetPackageName(): ?string
     {
         return 'codewithdennis/filament-advanced-choice';
-    }
-
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('filament-advanced-choice', __DIR__ . '/../resources/dist/components/filament-advanced-choice.js'),
-            Css::make('filament-advanced-choice-styles', __DIR__ . '/../resources/dist/filament-advanced-choice.css'),
-            Js::make('filament-advanced-choice-scripts', __DIR__ . '/../resources/dist/filament-advanced-choice.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            FilamentAdvancedChoiceCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getMigrations(): array
-    {
-        return [
-            'create_filament-advanced-choice_table',
-        ];
     }
 }
