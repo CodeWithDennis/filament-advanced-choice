b9ce3e53149557e6c4a32b760aaceff6e0532440
CodeWithDennis <personal.elsinga@gmail.com>
2025-07-05 19:47:10 +0200

    Add HasColumns trait to CheckboxCards, CheckboxStackedCards, RadioCards, and RadioStackedCards

--- src/Filament/Forms/Components/CheckboxCards.php
+++ src/Filament/Forms/Components/CheckboxCards.php
@@ -6,11 +6,13 @@ namespace CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components;
 
 use CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns\HasExtras;
 use CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns\HasHiddenInputs;
+use Filament\Schemas\Concerns\HasColumns;
 use Filament\Support\Concerns\HasColor;
 
 final class CheckboxCards extends \Filament\Forms\Components\CheckboxList
 {
     use HasColor;
+    use HasColumns;
     use HasExtras;
     use HasHiddenInputs;
 
--- src/Filament/Forms/Components/RadioCards.php
+++ src/Filament/Forms/Components/RadioCards.php
@@ -7,11 +7,13 @@ namespace CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components;
 use CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns\HasExtras;
 use CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns\HasHiddenInputs;
 use Filament\Forms\Components\Radio;
+use Filament\Schemas\Concerns\HasColumns;
 use Filament\Support\Concerns\HasColor;
 
 final class RadioCards extends Radio
 {
     use HasColor;
+    use HasColumns;
     use HasExtras;
     use HasHiddenInputs;
 
