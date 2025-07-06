4347720dfee8be26d6e5cecb0a6881843a1e8387
CodeWithDennis <personal.elsinga@gmail.com>
2025-07-06 16:15:32 +0200

    Add extra data to checkbox table items

--- resources/views/checkbox-table.blade.php
+++ resources/views/checkbox-table.blade.php
@@ -4,6 +4,7 @@
     use Filament\Support\Facades\FilamentView;
 
     $descriptions = $getDescriptions();
+    $extras = $getExtras();
     $colors = \Illuminate\Support\Arr::toCssStyles([
         get_color_css_variables($getColor(), shades: [50, 100, 200, 400, 500, 600, 700, 800]),
     ]);
@@ -91,67 +92,72 @@
                 @php
                     $id = $getId() . '-' . str($value)->slug();
                     $description = $descriptions[$value] ?? '';
+                    $extra = $extras[$value] ?? null;
                 @endphp
 
-                <label
-                    @if ($isSearchable)
+                                    <label
                         wire:key="{{ $livewireKey }}.options.{{ $value }}"
-                        x-show="
-                            $el
-                                .querySelector('.fi-fo-checkbox-list-option-label')
-                                ?.innerText.toLowerCase()
-                                .includes(search.toLowerCase()) ||
+                        @if ($isSearchable)
+                            x-show="
                                 $el
-                                    .querySelector('.fi-fo-checkbox-list-option-description')
+                                    .querySelector('.fi-fo-checkbox-list-option-label')
                                     ?.innerText.toLowerCase()
-                                    .includes(search.toLowerCase())
-                        "
-                    @endif
-                        for="{{ $id }}"
-                        class="fi-fo-checkbox-list-option group flex flex-col border border-gray-200 dark:border-gray-700 p-4
-                               first:rounded-tl-md first:rounded-tr-md last:rounded-br-md last:rounded-bl-md
-                               focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-custom-600
-                               has-checked:relative has-checked:border-custom-200 dark:has-checked:border-custom-500
-                               has-checked:bg-custom-50 dark:has-checked:bg-custom-800/10
-                               has-disabled:opacity-60
-                               md:grid md:grid-cols-2 md:pr-6 md:pl-4"
-                        style="{{ $colors }}"
-                    >
-                        <span class="fi-fo-checkbox-list-option-text flex items-center gap-3 text-sm">
-                            <input
-                                id="{{ $id }}"
-                                name="{{ $getName() }}"
-                                type="checkbox"
-                                value="{{ $value }}"
-                                {{
-                                    $extraInputAttributeBag
-                                        ->merge([
-                                            'disabled' => $isDisabled || $isOptionDisabled($value, $label),
-                                            'wire:loading.attr' => 'disabled',
-                                            $wireModelAttribute => $statePath,
-                                            'x-on:change' => $isBulkToggleable ? 'checkIfAllCheckboxesAreChecked()' : null,
-                                        ], escape: false)
-                                        ->class([
-                                            'fi-checkbox-input mt-0.5 shrink-0 checked:bg-custom-500 checked:border-custom-500 hover:checked:bg-custom-600 hover:checked:border-custom-600 focus:border-custom-500 focus:ring-custom-500',
-                                            'fi-valid' => ! $errors->has($statePath),
-                                            'fi-invalid' => $errors->has($statePath),
-                                        ])
-                                }}
-                                style="{{ $colors }}"
-                            />
-                            <span class="fi-fo-checkbox-list-option-label font-medium text-gray-900 dark:text-gray-100">
-                                @if ($isHtmlAllowed)
-                                    {!! $label !!}
-                                @else
-                                    {{ $label }}
-                                @endif
-                            </span>
-                        </span>
+                                    .includes(search.toLowerCase()) ||
+                                    $el
+                                        .querySelector('.fi-fo-checkbox-list-option-description')
+                                        ?.innerText.toLowerCase()
+                                        .includes(search.toLowerCase())
+                            "
+                        @endif
+                            for="{{ $id }}"
+                            class="fi-fo-checkbox-list-option group flex flex-col border border-gray-200 dark:border-gray-700 p-4
+                                   first:rounded-tl-md first:rounded-tr-md last:rounded-br-md last:rounded-bl-md
+                                   focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-custom-600
+                                   has-checked:relative has-checked:border-custom-200 dark:has-checked:border-custom-500
+                                   has-checked:bg-custom-50 dark:has-checked:bg-custom-800/10
+                                   has-disabled:opacity-60
+                                   md:grid md:grid-cols-3 md:items-center md:pr-6 md:pl-4"
+                            style="{{ $colors }}"
+                        >
+                            <div class="flex items-center gap-3 text-sm">
+                                <input
+                                    id="{{ $id }}"
+                                    name="{{ $getName() }}"
+                                    type="checkbox"
+                                    value="{{ $value }}"
+                                    {{
+                                        $extraInputAttributeBag
+                                            ->merge([
+                                                'disabled' => $isDisabled || $isOptionDisabled($value, $label),
+                                                'wire:loading.attr' => 'disabled',
+                                                $wireModelAttribute => $statePath,
+                                                'x-on:change' => $isBulkToggleable ? 'checkIfAllCheckboxesAreChecked()' : null,
+                                            ], escape: false)
+                                            ->class([
+                                                'fi-checkbox-input mt-0.5 shrink-0 checked:bg-custom-500 checked:border-custom-500 hover:checked:bg-custom-600 hover:checked:border-custom-600 focus:border-custom-500 focus:ring-custom-500',
+                                                'fi-valid' => ! $errors->has($statePath),
+                                                'fi-invalid' => $errors->has($statePath),
+                                            ])
+                                    }}
+                                    style="{{ $colors }}"
+                                />
+                                <span class="fi-fo-checkbox-list-option-label font-medium text-gray-900 dark:text-gray-100">
+                                    @if ($isHtmlAllowed)
+                                        {!! $label !!}
+                                    @else
+                                        {{ $label }}
+                                    @endif
+                                </span>
+                            </div>
 
-                        <span class="fi-fo-checkbox-list-option-description ml-6 pl-1 text-sm text-gray-500 dark:text-gray-400 md:ml-0 md:pl-0 md:text-right">
-                            {{ $description }}
-                        </span>
-                    </label>
+                            <div class="fi-fo-checkbox-list-option-description text-sm text-gray-500 dark:text-gray-400 md:text-left">
+                                {{ $description }}
+                            </div>
+
+                            <div class="fi-fo-checkbox-list-option-extra text-sm text-gray-500 dark:text-gray-400 md:text-right">
+                                {{ $extra }}
+                            </div>
+                        </label>
                 @endforeach
         </fieldset>
 
