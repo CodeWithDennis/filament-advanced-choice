a9010d20f9e82b77017b4e648a6d069a0c8792b6
Shaheen <shaheenfawzy1234@gmail.com>
2026-05-02 00:31:20 +0300

    feat: add not-allowed cursor on disabled options
    Disabled options already get has-disabled:opacity-60 to dim visually,
    but the cursor remained the default arrow on hover, indistinguishable
    from enabled options. Pair the existing opacity dim with
    has-disabled:cursor-not-allowed across all eight components so the
    cursor signals inertness on hover.
    
    Affects: RadioList, RadioTable, RadioCards, RadioStackedCards,
    CheckboxList, CheckboxTable, CheckboxCards, CheckboxStackedCards.

--- resources/views/radio-cards.blade.php
+++ resources/views/radio-cards.blade.php
@@ -77,7 +77,7 @@
                             .includes(search.toLowerCase())
                 " @endif class="fi-fo-checkbox-list-option-ctn">
                     <label for="{{ $id }}"
-                        class="fi-fo-checkbox-list-option group relative flex rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 p-4 has-checked:outline-2 has-checked:-outline-offset-1 has-checked:outline-custom-600 dark:has-checked:outline-custom-500 has-focus-visible:outline-3 has-focus-visible:-outline-offset-1 has-disabled:opacity-60"
+                        class="fi-fo-checkbox-list-option group relative flex rounded-lg border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900 p-4 has-checked:outline-2 has-checked:-outline-offset-1 has-checked:outline-custom-600 dark:has-checked:outline-custom-500 has-focus-visible:outline-3 has-focus-visible:-outline-offset-1 has-disabled:opacity-60 has-disabled:cursor-not-allowed"
                         style="{{ $colors }}">
                         @if($hiddenInputs)
                                     <input id="{{ $id }}" name="{{ $statePath }}" type="radio" value="{{ $value }}" {{
