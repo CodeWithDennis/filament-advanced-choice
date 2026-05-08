68d6223c19d6694e40e80ced0aaaacdf16275fb4
CodeWithDennis <personal.elsinga@gmail.com>
2026-05-08 16:24:54 +0200

    feat(components): add singular Card classes and deprecate plural names

---
+++ src/Filament/Forms/Components/RadioStackedCard.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components;
+
+use CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns\HasCursorPointer;
+use CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns\HasExtras;
+use CodeWithDennis\FilamentAdvancedChoice\Filament\Forms\Components\Concerns\HasHiddenInputs;
+use Filament\Forms\Components\Concerns\CanBeSearchable;
+use Filament\Forms\Components\Concerns\HasGridDirection;
+use Filament\Forms\Components\Radio;
+use Filament\Schemas\Concerns\HasColumns;
+use Filament\Support\Concerns\HasColor;
+use Filament\Support\Enums\GridDirection;
+
+class RadioStackedCard extends Radio
+{
+    use CanBeSearchable;
+    use HasColor;
+    use HasColumns;
+    use HasCursorPointer;
+    use HasExtras;
+    use HasGridDirection;
+    use HasHiddenInputs;
+
+    protected string $view = 'filament-advanced-choice::radio-stacked-cards';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->color('primary')
+            ->gridDirection(GridDirection::Row);
+    }
+}
